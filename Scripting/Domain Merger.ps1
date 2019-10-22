1d24d94824994f851980b1bce7c0592ae97579e9
Taha Ghazali <54667112+TahaOps@users.noreply.github.com>
2019-10-22 14:32:17 +0200

    Add files via upload
    Initial commit. 
    Works on WS2012R2
    Warning a bug is still there on this version : the third script is meant to be launched from $env:USERPROFILE\ but since the second script adds you in the Domain, the "C:\Users\Administrator" turns out to be "C:\Users\Administrator.DOMAIN"
    Therefore, the next version will create, in the first script, both an installation folder no more linked to the User but rather on a specific folder.

---
+++ Scripting/Domain Merger.ps1
@@ -0,0 +1,21 @@
+﻿<#
+Author : Taha Ghazali
+Creation Date : 2019-10-11
+Version : 1.1.1
+Edition Date : 2019-10-22
+Works with : PS 4.0
+#>
+
+Write-Host "Now we will $env:computername to the Forest and restart the machine"
+$DomainName = Read-Host -Prompt "Please enter the name of the domain "
+Add-Computer -Computername $env:computername -DomainName $DomainName –Credential $DomainName\Administrator –Force #-Restart
+# END MESSAGE
+Write-Host "Script ended."
+
+# CRON TASK
+Write-Host "Now we are scheduling a new script at boot in order to add roles on this computer ($ComputerName)"
+Unregister-ScheduledTask -TaskName "Domain Merger"
+$Step3 = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument "-File '$env:USERPROFILE\Role Installation.ps1'"
+$trigger = New-ScheduledTaskTrigger -AtLogon
+Register-ScheduledTask -TaskName "Role Installation" -Action $Step3 -Trigger $trigger
+Restart-Computer
---
+++ Scripting/Role Installation.ps1
@@ -0,0 +1,19 @@
+﻿<#
+Author : Taha Ghazali
+Creation Date : 2019-10-11
+Version : 1.1.4
+Edition Date : 2019-10-22
+Works with : PS 4.0
+#>
+Clear-Variable Roles*
+$RolesSearch = Read-Host -Prompt "Enter the roles you want to install on this server "
+Get-WindowsFeature | Where{$_.Name -like "*$RolesSearch*" -AND $_.InstallState -eq "Available"}
+$RolesNumber = Read-Host -Prompt "Enter the number of wanted roles "
+$RolesArray = @()
+For($i=1; $i -le $RolesNumber; $i++){
+$RolesName = Read-Host -Prompt "Enter the name of the role "
+$RolesArray += "$RolesName"
+}
+Install-WindowsFeature -Name $RolesArray
+Unregister-ScheduledTask -TaskName "Role Installation"
+Restart-Computer
